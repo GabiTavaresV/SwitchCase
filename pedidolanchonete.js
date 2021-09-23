0f88bbcbf6c6ac7403b652090ca08c1ea8227966
gabriela.ttavares <gabriela.ttavares@gmail.com>
2021-09-22 21:15:28 -0300

    Exemplo usando Switch Case

---
+++ pedidolanchonete.js
@@ -0,0 +1,26 @@
+var pedido = 20;
+var quantidade = 2;
+
+var total 
+
+switch (pedido) {
+    case 10:
+        total = 12.50 * quantidade;
+        console.log(`Você pediu Cachorro quente e vai pagar ${total}`)
+        break
+    case 20:
+        total = 16.50 * quantidade;
+        console.log(`Você pediu X-Burger e vai pagar ${total}`)
+        break
+    case 30:
+        total = 18.50 * quantidade;
+        console.log(`Você pediu Moda da Casa e vai pagar ${total}`)
+        break
+    case 30:
+        total = 22.50 * quantidade;
+        console.log(`Você pediu X-Tudo e vai pagar ${total}`)
+        break
+
+    default:
+      console.log("Código não existe!");
+}
